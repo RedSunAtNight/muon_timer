40dce3ef12d41883b6b36f195df82e95dd1977ce
krlynch <klynch@york.cuny.edu>
2017-01-03 18:35:32 -0500

    Added: init and exit boilerplate to create chrdev, class, and device
    enable insmod support for debug flag

--- BBB_Kernel_Driver/muon_timer.c
+++ BBB_Kernel_Driver/muon_timer.c
@@ -1,39 +1,86 @@
-/*
- * Linux 2.6 and later 'muon_timer' device driver
- *
- * Copyright (c) 2016-2017 Kevin Lynch <klynch@york.cuny.edu>
- *
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Kevin Lynch");
 MODULE_DESCRIPTION("An interrupt-based timing measurement driver for the BBB");
-MODULE_VERSION("0.1");
+MODULE_VERSION("1.0");
+
+// Macros swiped from parrot driver
+#define dbg(format, arg...) do { if (debug) pr_info(CLASS_NAME ": %s: " format , __FUNCTION__ , ## arg); } while (0)
+#define err(format, arg...) pr_err(CLASS_NAME ": " format, ## arg)
+#define info(format, arg...) pr_info(CLASS_NAME ": " format, ## arg)
+#define warn(format, arg...) pr_warn(CLASS_NAME ": " format, ## arg)
+
+#define DEVICE_NAME "muon_timer_device"
+#define CLASS_NAME "muon_timer"
 
 
+// module parameters appear in /sys/module/muon_timer/parameters
+// add 'debug=1' on insmod to change .. 
+static bool debug = false;
+module_param(debug, bool, S_IRUGO);
+MODULE_PARM_DESC(debug, "enable debug info (default: false)");
+// static int gpio_reset pin
+// static int gpio_pulse pin
+// static in gpio_input pin
+
+
+static int muon_major;
+static struct class *muon_class = 0;
+static struct device *muon_device = 0;
+
+static struct file_operations fops = {};
 
 static int __init muon_timer_init(void){
+  int retval = 0;
+
+  dbg("");
+  // Register char device and get a muon_major number
+  muon_major = register_chrdev(0, DEVICE_NAME, &fops);
+  if( muon_major<0 ){
+    err("failed to register char device: error %d\n", muon_major);
+    retval = muon_major;
+    goto failed_chrdev;
+  }
+
+  // Register the muon_timer virtual class ... no physical bus to
+  // connect to
+  muon_class = class_create(THIS_MODULE, CLASS_NAME);
+  if(IS_ERR(muon_class)){
+    err("failed to register device class '%s'\n", CLASS_NAME);
+    retval = PTR_ERR(muon_class);
+    goto failed_class;
+  }
+
+  // create device and nodes in /dev
+  muon_device = device_create(muon_class, NULL, MKDEV(muon_major, 0),
+			      NULL, CLASS_NAME);
+  if(IS_ERR(muon_device)){
+    err("failed to create device '%s'\n", CLASS_NAME);
+    retval = PTR_ERR(muon_device);
+    goto failed_create;
+  }
+  
   return 0;
+
+ failed_create:
+  class_destroy(muon_class);
+ failed_class:
+  unregister_chrdev(muon_major, DEVICE_NAME);
+ failed_chrdev:
+  return retval;
 }
 
 static void __exit muon_timer_exit(void){
+  dbg("");
 
+  device_destroy(muon_class, MKDEV(muon_major,0));
+  class_destroy(muon_class);
+  unregister_chrdev(muon_major, DEVICE_NAME);
 }
 
 
