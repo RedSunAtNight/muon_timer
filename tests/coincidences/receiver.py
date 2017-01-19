d345ac09f01b3850dda0bac760aaed4ca01f41e9
Demo User <debian@beaglebone.localdomain>
2017-01-19 16:50:43 +0000

    Removed extraneous comment

---
+++ tests/coincidences/receiver.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import, division, print_function
+
+# This test code runs in "self triggering" mode; connect the pulse
+# gpio output to the input gpio input before running.
+
+# If you plan on piping the output somewhere, you must run the
+# interpreter with the -u flag (unbuffered stdio), or else it will sit
+# forever before doing anything
+
+import time
+from ctypes import *
+
+class timespec(Structure):
+    _fields_ = [('tv_sec', c_long), ('tv_nsec', c_long)]
+
+ts = timespec()
+
+# I don't like this ... why can't I open the attribute once,
+# and read forever? writing works unbuffered, why not reading
+def read_attr(filename):
+    with open(filename, 'r') as f:
+        s = f.readline().strip()
+        return s
+    
+print('#cnt, ts.tv_sec, ts.tv_nsec, ints, missed')
+with open('/dev/muon_timer', 'rb') as muons:
+    cnt = 0
+    while(True):
+        cnt+=1
+        muons.readinto(ts)
+        time.sleep(100./1000.)
+        print(cnt, ts.tv_sec, ts.tv_nsec)
