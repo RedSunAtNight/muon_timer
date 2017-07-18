1445e02d5c9c344e2d2f7b192b18b14d346e2dea
Helenka Casler <hcasler@gc.cuny.edu>
2017-07-18 16:39:07 -0400

    simple little http server, just responds with timestamps

---
+++ data_server/logger.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+from datetime import datetime
+import sys
+
+class Logger:
+    def __init__(self, filename):
+        self.filename = filename
+    
+    def setup(self):
+        try:
+            self.logfile = open(self.filename, 'a')
+            self.useStdout = False
+        except BaseException as ex:
+            self.logfile = sys.stdout
+            self.useStdout = True
+            raise ex
+
+    def debug(self, line):
+        stamp = datetime.now().isoformat()
+        self.logfile.write("{} DEBUG: {}\n".format(stamp, line))
+
+    def info(self, line):
+        stamp = datetime.now().isoformat()
+        self.logfile.write("{} INFO: {}\n".format(stamp, line))
+
+    def warn(self, line):
+        stamp = datetime.now().isoformat()
+        self.logfile.write("{} WARN: {}\n".format(stamp, line))
+
+    def error(self, line):
+        stamp = datetime.now().isoformat()
+        self.logfile.write("{} ERROR: {}\n".format(stamp, line))
+
+    def close(self):
+        if not self.useStdout:
+            self.logfile.close()
---
+++ data_server/server.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python
+
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from logger import Logger
+import time
+from ctypes import *
+
+# default value
+logfile = "/var/log/muon_timer/server.log"
+# this is stupid but it works
+log = Logger(logfile)
+
+class timespec(Structure):
+    _fields_ = [('tv_sec', c_long), ('tv_nsec', c_long)]
+
+class ReqHandler(BaseHTTPRequestHandler):
+
+    def _set_headers(self):
+        self.send_response(200)
+        #self.send_header('Transfer-Encoding', 'chunked')
+        self.send_header('Content-Type', 'text/html')
+        self.end_headers()
+
+    def _set_fail_headers(self, code, failInfo):
+        self.send_response(code)
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', str(len(failInfo)))
+        self.end_headers()
+
+    def do_GET(self):
+        log.info("Received request: "+self.requestline+" from "+str(self.client_address))
+        self._set_headers()
+        ts = timespec()
+        self.wfile.write('#cnt, ts.tv_sec, ts.tv_nsec')
+        try:
+            with open('/dev/muon_timer', 'rb') as muons:
+                cnt = 0
+                while(True):
+                    cnt+=1
+                    muons.readinto(ts)
+                    time.sleep(100./1000.)
+                    self.wfile.write("{} {} {}\n".format(cnt, ts.tv_sec, ts.tv_nsec))
+        except BaseException as ex:
+            msg = "ERROR: "+str(ex)+"\n"
+            log.error("Failed while writing response to "+str(self.client_address)+": "+msg)
+            self._set_fail_headers(500, msg)
+            self.wfile.write(msg)
+        log.info("response ended")
+
+    def do_HEAD(self):
+        self._set_headers()
+        
+    def do_POST(self):
+        msg = "POST requests not supported"
+        self._set_fail_headers(400, msg)
+        self.wfile.write(msg)
+
+def run(server_class=HTTPServer, handler_class=ReqHandler, port=8090):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print 'Starting httpd...'
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    logfile = "/home/helenka/Desktop/aLogFile.txt"
+    log.filename = logfile
+    try:
+        log.setup()
+    except BaseException as ex:
+        print ex
+    run()
